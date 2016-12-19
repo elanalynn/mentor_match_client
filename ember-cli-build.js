b84f37f569b9354e57a418a34ee7648408cb7563
Elana Kopelevich <ekopelevich@gmail.com>
2016-12-19 11:51:21 -0700

    install sass, bootstrap, create navbar

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,12 +4,24 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    // sassOptions: {
+    //   sourceMap: true
+    // }
+    // outputPaths: {
+    //   app: {
+    //     css: {
+    //       'app': '/assets/mm-client.css'
+    //     }
+    //   }
+    // }
   });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
-  //
+
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
   // object's keys should be the environment name and the values
