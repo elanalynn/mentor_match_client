6725ab46d2271235f25101093d582db74f0a22d6
Elana Kopelevich <ekopelevich@gmail.com>
2016-12-22 14:38:38 -0700

    add deployed url to whitelist

--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ module.exports = function(environment) {
     },
 
     fastboot: {
-      hostWhitelist: [/^localhost:\d+$/]
+      hostWhitelist: ['ek-mm-client.herokuapp.com', /^localhost:\d+$/]
     }
   };
 
