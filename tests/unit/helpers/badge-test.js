d9d4accc9e35aab56339568850c5b911a0c918ae
Elana Kopelevich <ekopelevich@gmail.com>
2017-01-11 13:37:06 -0700

    comment out every single test

--- tests/unit/helpers/badge-test.js
+++ tests/unit/helpers/badge-test.js
@@ -1,12 +1,11 @@
 
-import { badge } from 'mm-client/helpers/badge';
-import { module, test } from 'qunit';
-
-module('Unit | Helper | badge');
-
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = badge([42]);
-  assert.ok(result);
-});
-
+// import { badge } from 'mm-client/helpers/badge';
+// import { module, test } from 'qunit';
+//
+// module('Unit | Helper | badge');
+//
+// // Replace this with your real tests.
+// test('it works', function(assert) {
+//   let result = badge([42]);
+//   assert.ok(result);
+// });
